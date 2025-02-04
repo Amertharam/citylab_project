cb08a291c69740a65de8b196ed148cee2df3eff4
Amertharam <amertharamprime@gmail.com>
2025-02-04 18:15:17 +0000

    Checkpoint 5: Fixed(always maintain 0.1m/s forward linear vel, apply angular vel only when obstacle detected)

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,76 +1,91 @@
 #include <rclcpp/rclcpp.hpp>
+#include "rclcpp/logging.hpp"
+#include "rclcpp/subscription_options.hpp"
+#include "rclcpp/callback_group.hpp"
+#include "rclcpp/executors/multi_threaded_executor.hpp"
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <geometry_msgs/msg/twist.hpp>
-#include <cmath>
+#include <algorithm>
 #include <limits>
+#include <chrono>
 
 using namespace std::chrono_literals;
 
 class Patrol : public rclcpp::Node {
 public:
   Patrol() : Node("patrol_node") {
-    scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        "scan", 10, std::bind(&Patrol::scan_callback, this, std::placeholders::_1));
-    cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 100);
-    timer_ = this->create_wall_timer(
-        100ms, std::bind(&Patrol::control_loop, this));
+    patrol_group = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+    rclcpp::SubscriptionOptions patrol_opt;
+    patrol_opt.callback_group = patrol_group;
+    cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    laser_sub_ = create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&Patrol::laserCallback, this, std::placeholders::_1), patrol_opt);
+
+    control_timer = this->create_wall_timer(100ms, std::bind(&Patrol::controlLoop, this), patrol_group);
+    msg_timer     = this->create_wall_timer(500ms, std::bind(&Patrol::infoMsg, this), patrol_group);
+
+    cmd.linear.x = 0.0f;
+    cmd.angular.z = 0.0f;
+  }
+  ~Patrol() {
+    RCLCPP_INFO(this->get_logger(), "Destructor Called Patrol Node Destroyed");
   }
 
 private:
-  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    scan_ = msg;
-  }
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        // Front sector detection (center ±50 samples)
+        size_t center = msg->ranges.size() / 2;
+        auto front_sector = std::vector<float>(
+            msg->ranges.begin() + center - 50,
+            msg->ranges.begin() + center + 50
+        );
+        
+        // Obstacle detection
+        obstacle_detected_ = std::any_of(front_sector.begin(), front_sector.end(),
+            [](float dist){ return std::isfinite(dist) && dist < 0.35; });
 
-  void control_loop() 
-  {
-   if(scan_ == nullptr){ return; }
+        // Full 180° processing (-90° to +90°)
+        auto front_180 = std::vector<float>(
+            msg->ranges.begin() + msg->ranges.size()/4,
+            msg->ranges.begin() + 3*msg->ranges.size()/4
+        );
 
-   geometry_msgs::msg::Twist cmd;
-   cmd.linear.x = FORWARD_SPEED;
-
-   float safest_angle = find_safest_angle();
-
-   if (std::isfinite(safest_angle) && safest_angle < 3.5) 
-   { 
-    cmd.angular.z = std::clamp(-safest_angle * ANGULAR_SPEED_GAIN, -MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED);
-   } 
-   else 
-   {
-    cmd.angular.z = 0.0;
-   }
-   cmd_pub_->publish(cmd);
- }
-
-  float find_safest_angle() {
-    float max_distance = 0.0;
-    float safest_angle = std::numeric_limits<float>::quiet_NaN(); 
-
-    for (int i = -SCAN_ANGLE_RANGE / 2; i <= SCAN_ANGLE_RANGE / 2; ++i) {
-      int index = (i + 360) % 360; 
-      float distance = scan_->ranges[index];
-
-      if (std::isfinite(distance) && distance > max_distance) {
-        max_distance = distance;
-        safest_angle = i * scan_->angle_increment; 
-      }
+        // Find safest direction
+        float max_dist = 0;
+        size_t max_index = 0;
+        for(size_t i = 0; i < front_180.size(); ++i) {
+            if(front_180[i] > max_dist && std::isfinite(front_180[i])) {
+                max_dist = front_180[i];
+                max_index = i;
+            }
+        }
+        direction_ = (static_cast<float>(max_index)/front_180.size() - 0.5) * M_PI;
     }
 
-    return safest_angle;
-  }
+    void controlLoop() {       
+        cmd.linear.x = 0.1; // To always maintain 0.1 m/s forward velocity. I fixed it as a constant value.            
+        cmd.angular.z = obstacle_detected_ ? (direction_ / 2) : 0.0;    // Apply angular velocity only when obstacle detected        
+        cmd_vel_pub_->publish(cmd);
+    }
+    void infoMsg() {
+      RCLCPP_INFO(this->get_logger(), "Linear Vel:  %f | Angular Vel : %f", cmd.linear.x, cmd.angular.z);
+    }
 
-  const float FORWARD_SPEED = 0.1;
-  const float MAX_ANGULAR_SPEED = 1.0;
-  const float ANGULAR_SPEED_GAIN = 0.5;
-  const int SCAN_ANGLE_RANGE = 180; 
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  sensor_msgs::msg::LaserScan::SharedPtr scan_;
+    geometry_msgs::msg::Twist cmd;
+    rclcpp::CallbackGroup::SharedPtr patrol_group;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    rclcpp::TimerBase::SharedPtr control_timer;
+    rclcpp::TimerBase::SharedPtr msg_timer;
+    float direction_ = 0.0;
+    bool obstacle_detected_ = false;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Patrol>());
+  std::shared_ptr<Patrol> patrol_node= std::make_shared<Patrol>();
+  rclcpp::executors::MultiThreadedExecutor execute;
+  execute.add_node(patrol_node);
+  execute.spin();
   rclcpp::shutdown();
   return 0;
 }
